e4002ccf20f8fe711fb6dacf364517fd7607e963
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-03 23:56:56 +0700

    Delete models/_init_.py

--- models/_init_.py
+++
@@ -1,6 +0,0 @@
-"""Модели данных"""
-from .session import UserSession, SessionStatus
-from .enums import ConversationState, NicheCategory
-from .question_types import QuestionType
-
-__all__ = ['UserSession', 'SessionStatus', 'ConversationState', 'NicheCategory', 'QuestionType']
