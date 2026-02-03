a4cb977bbb175d17944ca2b26e534d13c75556a7
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-03 23:56:26 +0700

    Delete services/_init_.py

--- services/_init_.py
+++
@@ -1,6 +0,0 @@
-"""Сервисы бота"""
-from .data_manager import DataManager, data_manager
-from .openai_service import OpenAIService
-from .payment_service import PaymentService
-
-__all__ = ['DataManager', 'data_manager', 'OpenAIService', 'PaymentService']
