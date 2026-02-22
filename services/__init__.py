0f09b34cdb741c5a76eb2268e6cd053f7c2ddd09
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-22 23:47:01 +0700

    Fix string quotes in __all__ declaration

--- services/__init__.py
+++ services/__init__.py
@@ -3,4 +3,4 @@ from .data_manager import DataManager, data_manager
 from .openai_service import OpenAIService, openai_service
 from .payment_service import PaymentService
 
-__all__ = ['DataManager', 'data_manager', 'OpenAIService', 'openai_service', 'PaymentService']
+__all__ = ["DataManager", "data_manager", "OpenAIService", "openai_service", "PaymentService"]
