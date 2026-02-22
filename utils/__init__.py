4079b310cbf89a5936256132c31fde064908af7d
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-22 23:33:34 +0700

    Add additional logging functions to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,12 @@
 """Утилиты"""
-from .logger import setup_logging
+from .logger import setup_logging, get_logger, log_info, log_warning, log_error, log_debug
 from .formatters import *
 
-__all__ = ['setup_logging']
+__all__ = [
+    'setup_logging',
+    'get_logger',
+    'log_info',
+    'log_warning',
+    'log_error',
+    'log_debug'
+]
