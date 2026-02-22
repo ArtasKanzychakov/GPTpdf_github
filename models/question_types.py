4173fcb3a91b5a95ed1af56b01728ce7eaae2b8a
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-22 23:20:53 +0700

    Refactor question types to remove extra whitespace

--- models/question_types.py
+++ models/question_types.py
@@ -3,37 +3,33 @@
 """
 from enum import Enum
 
-
 class QuestionType(Enum):
     """Типы вопросов в анкете"""
-    
     # Простые типы
-    TEXT = "text"  # Текстовый ответ
-    CHOICE = "choice"  # Одиночный выбор (кнопки)
-    MULTIPLE_CHOICE = "multiple_choice"  # Множественный выбор
+    TEXT = "text"
+    CHOICE = "choice"
+    MULTIPLE_CHOICE = "multiple_choice"
     
     # Интерактивные типы (новые)
-    QUICK_BUTTONS = "quick_buttons"  # Быстрые кнопки (Q1, Q2, Q3)
-    MULTI_SELECT = "multi_select"  # Мультиселект с лимитом
-    SCENARIO_TEST = "scenario_test"  # Интерактивный сценарий (Q5)
-    SLIDER_WITH_SCENARIO = "slider_with_scenario"  # Сценарий + ползунок (Q6)
-    ENERGY_DISTRIBUTION = "energy_distribution"  # 3 ползунка (Q7)
-    SKILL_RATING = "skill_rating"  # Рейтинг по звездам (Q9)
-    SUPERHERO_METAPHOR = "superhero_metaphor"  # Метафорический выбор (Q10)
-    LEARNING_ALLOCATION = "learning_allocation"  # Распределение баллов (Q12)
-    EXISTENTIAL_TEXT = "existential_text"  # Развернутый ответ (Q13)
-    FLOW_EXPERIENCE = "flow_experience"  # Пример + описание (Q14)
-    CLIENT_PORTRAIT = "client_portrait"  # Портрет клиента (Q15)
-
+    QUICK_BUTTONS = "quick_buttons"
+    MULTI_SELECT = "multi_select"
+    SCENARIO_TEST = "scenario_test"
+    SLIDER_WITH_SCENARIO = "slider_with_scenario"
+    ENERGY_DISTRIBUTION = "energy_distribution"
+    SKILL_RATING = "skill_rating"
+    SUPERHERO_METAPHOR = "superhero_metaphor"
+    LEARNING_ALLOCATION = "learning_allocation"
+    EXISTENTIAL_TEXT = "existential_text"
+    FLOW_EXPERIENCE = "flow_experience"
+    CLIENT_PORTRAIT = "client_portrait"
 
 class QuestionCategory(Enum):
     """Категории вопросов"""
-    DEMOGRAPHIC = "demographic"  # Q1-Q3
-    PERSONALITY = "personality"  # Q4-Q8
-    SKILLS = "skills"  # Q9-Q12
-    VALUES = "values"  # Q13-Q15
-    RESOURCES = "resources"  # Q16-Q18
-
+    DEMOGRAPHIC = "demographic"
+    PERSONALITY = "personality"
+    SKILLS = "skills"
+    VALUES = "values"
+    RESOURCES = "resources"
 
 class AnswerValidationRule(Enum):
     """Правила валидации ответов"""
@@ -42,5 +38,5 @@ class AnswerValidationRule(Enum):
     MAX_LENGTH = "max_length"
     MIN_CHOICES = "min_choices"
     MAX_CHOICES = "max_choices"
-    RANGE = "range"  # Для чисел/слайдеров
-    SUM_EQUALS = "sum_equals"  # Для распределения баллов
+    RANGE = "range"
+    SUM_EQUALS = "sum_equals"
