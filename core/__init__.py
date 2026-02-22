38577a63cc002a81ef5627d4b5ca108321d20f44
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-22 23:48:08 +0700

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -2,4 +2,4 @@
 from .bot import BusinessNavigatorBot
 from .question_engine_v2 import QuestionEngineV2
 
-__all__ = ['BusinessNavigatorBot', 'QuestionEngineV2']
+__all__ = ["BusinessNavigatorBot", "QuestionEngineV2"]
