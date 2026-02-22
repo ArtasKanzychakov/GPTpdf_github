4680bd73c4735e9fb13f1c451d0600a4c13449b2
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-22 23:22:44 +0700

    Add command and questionnaire handlers to init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,18 @@
 """Обработчики команд и сообщений"""
+from .commands import (
+    start_command,
+    help_command,
+    status_command,
+    restart_command,
+    questionnaire_command
+)
+from .questionnaire import questionnaire_handler
+
+__all__ = [
+    'start_command',
+    'help_command',
+    'status_command',
+    'restart_command',
+    'questionnaire_command',
+    'questionnaire_handler'
+]
