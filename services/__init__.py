e37ab63f3c13d971c08341b83816849d53449a29
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-22 23:30:08 +0700

    Add openai_service to module exports

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,6 @@
 """Сервисы бота"""
 from .data_manager import DataManager, data_manager
-from .openai_service import OpenAIService
+from .openai_service import OpenAIService, openai_service
 from .payment_service import PaymentService
 
-__all__ = ['DataManager', 'data_manager', 'OpenAIService', 'PaymentService']
+__all__ = ['DataManager', 'data_manager', 'OpenAIService', 'openai_service', 'PaymentService']
