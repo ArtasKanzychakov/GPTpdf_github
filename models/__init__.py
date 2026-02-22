270d74b43353af0846563432cc10bd565b9d1d84
ArtasKanzychakov <Artasmovie@yandex.ru>
2026-02-22 23:19:35 +0700

    Add NicheDetails to models initialization

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 """
 Модели данных для Business Navigator
 """
-from .session import UserSession, SessionStatus, DemographicData
+from .session import UserSession, SessionStatus, DemographicData, NicheDetails
 from .enums import ConversationState, PaymentStatus, NicheCategory, UserRole
 from .question_types import QuestionType, QuestionCategory, AnswerValidationRule
 
@@ -9,6 +9,7 @@ __all__ = [
     "UserSession",
     "SessionStatus", 
     "DemographicData",
+    "NicheDetails",
     "ConversationState",
     "PaymentStatus",
     "NicheCategory",
